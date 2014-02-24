779aa579c0080ceb42d73c1dee4e8ab79df15444
Keesjan de Vries <keesjan777@gmail.com>
2014-02-24 17:28:32 +0000

    convert from barn no milibarn

--- python/make_filemap_dict.py
+++ python/make_filemap_dict.py
@@ -4,12 +4,15 @@ import os
 def filemap_from_single_diroctory(dir,rootfile,experiment):
     #UGLY but this works
     (err, xsec) = commands.getstatusoutput('grep "TOTAL" '+dir+'/pythia-output.log | grep -o "[0-9]\{1,100\}\.[0-9]\{1,100\}[E\-]\{0,2\}[0-9]\{0,3\}"')
+    #very important: convert from milibarn to barn
+    xsec=float(xsec)*1e-3
+    print('XSEC: {0} barn'.format(xsec))
     #to be robust against giving the absolute path to the rootfile
     rootfile=os.path.basename(rootfile)
     rootfile_path=os.path.join(dir,rootfile)
     internal_name,_=os.path.splitext(rootfile)
     return {
-            'xsec':float(xsec),
+            'xsec':xsec,
             'rootfiles':[rootfile_path],
             'internal_name':internal_name,
             'experiment':experiment,
