b00319992e006f07db3de26c8be7fe4c24343a08
Keesjan de Vries <keesjan777@gmail.com>
2014-05-02 15:22:57 +0100

    remove good electrons, muons, jets funcions

--- src/analyses/analysis_cms_3_lepton_20fb.cc
+++ src/analyses/analysis_cms_3_lepton_20fb.cc
@@ -27,107 +27,23 @@ Cms3Lepton20Fb::Cms3Lepton20Fb(const std::string & name,
 		   const bool & calculateR) :
   AnalysisBase(name, experiment, numBins, intlumi, bgpred, bgpreduncert, datayields, fitmode, calculateR) {}
 
-
 Cms3Lepton20Fb::~Cms3Lepton20Fb() {}
-
-TSimpleArray<TRootJet> Cms3Lepton20Fb::SubArrayGoodJets(const TClonesArray *JET, const float & pt, const float & eta) {
-
-  TIter itJet((TCollection*)JET);
-  TRootJet *jet;
-  itJet.Reset();
-  TSimpleArray<TRootJet> array;
-  while( (jet = (TRootJet*) itJet.Next()) ) {
-    if(jet->PT > pt && fabs(jet->Eta) < eta) {
-      array.Add(jet);
-    }
-  }
-  return array;
-}
-
-TSimpleArray<TRootElectron> Cms3Lepton20Fb::SubArrayEl(const TClonesArray *ELEC, const float & pt, const float & eta) {
-  TIter itElec((TCollection*)ELEC);
-  TRootElectron *elec;
-  itElec.Reset();
-  TSimpleArray<TRootElectron> array;
-  while( (elec = (TRootElectron*) itElec.Next()) )
-    {
-      if(elec->PT<pt || !elec->IsolFlag || fabs(elec->Eta) > eta) continue;
-      array.Add(elec);
-    }
-  return array;
-}
-
-TSimpleArray<TRootMuon> Cms3Lepton20Fb::SubArrayMu(const TClonesArray *MUON, const float & pt, const float & eta) {
-  TIter itMuon((TCollection*)MUON);
-  TRootMuon *muon;
-  itMuon.Reset();
-  TSimpleArray<TRootMuon> array;
-  while( (muon = (TRootMuon*) itMuon.Next()) )
-    {
-      if(muon->PT<pt || !muon->IsolFlag || fabs(muon->Eta) > eta) continue;
-      array.Add(muon);
-    }
-  return array;
-}
-
-TSimpleArray<TRootTauJet> Cms3Lepton20Fb::SubArrayTauJet(const TClonesArray *TAUJET, const float & pt, const float & eta) {
-  TIter itTauJet((TCollection*)TAUJET);
-  TRootTauJet *taujet;
-  itTauJet.Reset();
-  TSimpleArray<TRootTauJet> array;
-  while( (taujet = (TRootTauJet*) itTauJet.Next()) )
-    {
-      if(taujet->PT<pt || !taujet->IsolFlag || fabs(taujet->Eta) > eta) continue;
-      array.Add(taujet);
-    }
-  return array;
-}
-
-TSimpleArray<TRootJet> Cms3Lepton20Fb::SubArrayBadJets(const TClonesArray *JET, const float & pt, const float & eta) {
-
-  TIter itJet((TCollection*)JET);
-  TRootJet *jet;
-  itJet.Reset();
-  TSimpleArray<TRootJet> array;
-  while( (jet = (TRootJet*) itJet.Next()) ) {
-    //check if any jet has Pt>50 and |eta|>3.0
-    if(jet->PT > pt && fabs(jet->Eta) > eta) {
-      array.Add(jet);
-    }
-  }
-  return array;
-}
-
-TSimpleArray<TRootETmis> Cms3Lepton20Fb::makeETM(const TClonesArray *ETMISS) {
-  TIter itEtMiss((TCollection*)ETMISS);
-  TRootETmis *etm;
-  itEtMiss.Reset();
-  TSimpleArray<TRootETmis> array;
-  while( (etm = (TRootETmis*) itEtMiss.Next()) ) {
-    array.Add(etm);
-  }
-  return array;
-}
-
 
 void Cms3Lepton20Fb::initHistos() {
   andir->cd();
 }
 
-void Cms3Lepton20Fb::Run(const TreeReader & treereader, const TreeReader & gentreereader, const double & weight) {
 
+void Cms3Lepton20Fb::Run(const Reader * treereader, const Reader * gentreereader, const double & weight) {
   andir->cd();
 
   mCounter+=weight; //keep a tally of all the files/events we are running over
 
   //produce subarrays of objects satisfying our criteria
-  TSimpleArray<TRootElectron> ele=SubArrayEl(treereader.Elec(), 10.0, 2.4);         //the central isolated electrons, pt > PT_ELEC GeV
-  TSimpleArray<TRootMuon>     mu=SubArrayMu(treereader.Muon(), 10.0, 2.4);          //the central isolated muons, pt > PT_MUON GeV
-  TSimpleArray<TRootJet>      badjets=SubArrayBadJets(treereader.Jet(),50.0,3.0);   //check for jets which we should veto
-  TSimpleArray<TRootJet>      goodjets275=SubArrayGoodJets(treereader.Jet(),37.0,3.0); //check for jets which we should analyse
-  TSimpleArray<TRootJet>      goodjets325=SubArrayGoodJets(treereader.Jet(),43.0,3.0); //check for jets which we should analyse
-  TSimpleArray<TRootJet>      goodjets375=SubArrayGoodJets(treereader.Jet(),50.0,3.0); //check for jets which we should analyse
-  TSimpleArray<TRootETmis>    etmis=makeETM(treereader.ETMis()); //Missing transverse energy array
+  std::vector<jlepton> ele=goodleptons(treereader->GetElec(), 10.0, 2.4,2.5,2.6);         //the central isolated electrons, pt > PT_ELEC GeV
+  std::vector<jlepton> mu=goodleptons(treereader->GetMuon(), 10.0, 2.1,2.5,2.6);          //the central isolated muons, pt > PT_MUON GeV
+  std::vector<jjet> badjets=badjetsSkim(treereader->GetJet(),50.0,3.0);   //check for jets which we should veto
+  std::vector<jjet> goodjets275=goodjetsSkim(treereader->GetJet(),37.0,3.0); //check for jets which we should analyse
 
   return;
 }
