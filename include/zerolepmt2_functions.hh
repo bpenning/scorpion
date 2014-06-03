fd2d5db22968ac824fb676066e3a0856904bdb58
Keesjan de Vries <keesjan777@gmail.com>
2014-06-03 12:18:31 +0100

    add a seperate file for the mt2 grouping

---
+++ include/zerolepmt2_functions.hh
@@ -0,0 +1,8 @@
+#ifndef __ZEROLEPMT2_FUNCTIONS__
+#define __ZEROLEPMT2_FUNCTIONS__
+#include <vector>
+#include <iostream>
+#include "jad_jet_class.hh"
+
+std::vector<int> getPseudoJetsGrouping(std::vector<jjet> );
+#endif
