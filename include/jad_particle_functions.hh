935c044c01b59c733a6849edd15ed614ff2807fb
Keesjan de Vries <keesjan777@gmail.com>
2014-06-13 14:33:28 +0100

    add getGenParticles that takes pid, status, pt, and eta as arguments

--- include/jad_particle_functions.hh
+++ include/jad_particle_functions.hh
@@ -1,6 +1,10 @@
 #ifndef __JPARTFUNC__
 #define __JPARTFUNC__
-
 #include "jad_particle_class.hh"
-std::vector<jparticle> getGenParticles(const std::vector<jparticle> & particle_collection, const unsigned int & PIDfilter);
+
+std::vector<jparticle> getGenParticles(const std::vector<jparticle> & 
+        particle_collection, const unsigned int & PIDfilter);
+std::vector<jparticle> getGenParticles(const std::vector<jparticle> & 
+        particle_collection, const unsigned int & PIDfilter, const int & status, 
+        const double & pt, const double & eta) ;
 #endif
--- src/analyses/jad_particle_functions.cc
+++ src/analyses/jad_particle_functions.cc
@@ -12,8 +12,8 @@ std::vector<jparticle> getGenParticles(const std::vector<jparticle> & particle_c
     }
     
     if(addparticle) {
-      if(particle_collection[ii].Status() == 2 && fabs(particle_collection[ii].Eta()) < 2.5 && particle_collection[ii].Pt() > 2.0) {
-	jparticle particle(particle_collection[ii].Px(), particle_collection[ii].Py(), particle_collection[ii].Pz(), particle_collection[ii].E(), particle_collection[ii].PID(), particle_collection[ii].Status(), particle_collection[ii].Charge());
+      if(particle_collection[ii].Status() == 3 && fabs(particle_collection[ii].Eta()) < 2.4 && particle_collection[ii].Pt() > 10.0) {
+	    jparticle particle(particle_collection[ii].Px(), particle_collection[ii].Py(), particle_collection[ii].Pz(), particle_collection[ii].E(), particle_collection[ii].PID(), particle_collection[ii].Status(), particle_collection[ii].Charge());
 	myparticles.push_back(particle);
       }
     }
@@ -21,4 +21,29 @@ std::vector<jparticle> getGenParticles(const std::vector<jparticle> & particle_c
 
   return myparticles;
 
+}
+
+std::vector<jparticle> getGenParticles(const std::vector<jparticle> & 
+        particle_collection, const unsigned int & PIDfilter, const int & status, 
+        const double & pt, const double & eta) {
+
+  std::vector<jparticle> myparticles;
+  for (int ii=0; ii<particle_collection.size();ii++){
+    bool addparticle = false;
+    if(PIDfilter == 0 || abs(particle_collection[ii].PID()) == PIDfilter) 
+      addparticle = true;
+  
+    if(addparticle) {
+      if(particle_collection[ii].Status() == status && 
+              fabs(particle_collection[ii].Eta()) < eta && 
+              particle_collection[ii].Pt() > pt) {
+	    jparticle particle(particle_collection[ii].Px(), 
+                particle_collection[ii].Py(), particle_collection[ii].Pz(), 
+                particle_collection[ii].E(), particle_collection[ii].PID(), 
+                particle_collection[ii].Status(), particle_collection[ii].Charge());
+	    myparticles.push_back(particle);
+      }
+    }
+  }
+  return myparticles;
 }
